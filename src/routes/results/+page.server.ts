40d3af6784f316a9b7944e58a6035f4a4e34b959
soundwanders <soundwanders@gmail.com>
2023-12-02 12:34:50 -0500

    create array of mascot names, use to filter school name from selected team strings for correct API call structure

--- src/routes/results/+page.server.ts
+++ src/routes/results/+page.server.ts
@@ -2,62 +2,83 @@
 import type { PageServerLoad } from './$types';
 import { CFBD_API_KEY } from '$env/static/private';
 import { getCurrentWeek } from '$lib/utils/getCurrentWeek';
+import { mascotNames } from '$lib/utils/mascots';
 
 export const load: PageServerLoad = async ({ params, url }) => {
-  try {
-    // Access the teams parameter from the query string
-    const teams = url.searchParams.get('teams');
-    const week = url.searchParams.get('week');
+	try {
+		// Access the teams parameter from the query string
+		const teams = url.searchParams.get('teams');
+		const week = url.searchParams.get('week');
 
-    // Ensure that teams is a string
-    const teamsString = teams || '';
+		// Ensure that teams is a string
+		const teamsString = teams || '';
 
-    // Split the teams string into an array
-    const teamArray = teamsString.split(',');
-    
-      // Extract the school name from the full team name to properly structure the API call
-      // For example, "Akron Zips" needs to be transformed into "Akron"
-    const schoolNamesArray = teamArray.map((fullTeamName: string) => {
-      return fullTeamName.split(' ')[0];
-    });
+		// Split the teams string into an array
+		const teamArray = teamsString.split(',');
 
-    // Create a variable to store the current year
-    const currentYear = new Date().getFullYear();
+		// Create a variable to store the current year, will be used in the fetch url
+		const currentYear = new Date().getFullYear();
 
-    // Use the selected week or default to the current week
-    const selectedWeek = week || getCurrentWeek();
+		// Use the selected week or default to the current week
+		const selectedWeek = week || getCurrentWeek();
 
-    // Make multiple API calls concurrently
-    const fetchGameResultsPromises = schoolNamesArray.map(async (team: string) => {
-      const url = `https://api.collegefootballdata.com/games?year=${currentYear}&week=${selectedWeek}&team=${team}`;
+		// Make multiple API calls concurrently using Promise.all
+		const fetchGameResultsPromises = teamArray.map(async (fullTeamName: string) => {
+			const schoolName = getSchoolName(fullTeamName);
+			const url = `https://api.collegefootballdata.com/games?year=${currentYear}&week=${selectedWeek}&team=${encodeURIComponent(
+				schoolName
+			)}`;
 
-      console.log('URL', url);
-      
-      const res = await fetch(url, {
-        headers: {
-          'Authorization': `Bearer ${CFBD_API_KEY}`,
-        },
-      });
+			try {
+				const res = await fetch(url, {
+					headers: {
+						Authorization: `Bearer ${CFBD_API_KEY}`
+					}
+				});
 
-      if (!res.ok) {
-        return { team, error: `Failed to fetch game results` };
-      }
+				if (!res.ok) {
+					throw new Error(`Failed to fetch game results for ${schoolName}. Status: ${res.status}`);
+				}
 
-      const data = await res.json();
+				const data = await res.json();
 
-      return { team, data };
-    });
+				return { team: schoolName, data };
+			} catch (error) {
+				console.error(error);
+				return { team: schoolName, error: `Failed to fetch game results for ${schoolName}` };
+			}
+		});
 
-    // Wait for all API calls to complete
-    const results = await Promise.all(fetchGameResultsPromises);
+		// Wait for all API calls to complete
+		const results = await Promise.all(fetchGameResultsPromises);
 
-    return {
-      gameResults: results,
-    }
-  } catch (error) {
-    console.error(error);
-    return {
-      error: 'Internal Server Error',
-    }
-  }
+		return {
+			gameResults: results
+		};
+	} catch (error) {
+		console.error(error);
+		return {
+			error: 'Internal Server Error'
+		};
+	}
+};
+
+// Function to extract the school mascots/team names from the full team name
+function getSchoolName(fullTeamName: string): string {
+	// Split the full team name into parts
+	const parts = fullTeamName.split(' ');
+
+	// Iterate over the parts to filter out mascots from the team name
+	let filteredParts: string[] = [];
+	for (let i = 0; i < parts.length; i++) {
+		const remainingSubstring = parts.slice(i).join(' ');
+
+		console.log(`Iteration ${i}: parts: ${parts}, remainingSubstring: ${remainingSubstring}`);
+
+		if (!mascotNames.includes(remainingSubstring)) {
+			filteredParts.push(parts[i]);
+		}
+	}
+
+	return filteredParts.join(' ');
 };
--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -1,179 +1,180 @@
 <!-- +page.svelte -->
 <script lang="ts">
-  import { selectedTeams } from '$lib/stores/store';
-  import { theme } from '$lib/stores/theme';
+	import { selectedTeams } from '$lib/stores/store';
+	import { theme } from '$lib/stores/theme';
 	import ResubmitSearch from '../../components/ResubmitSearch.svelte';
 	import '../../styles/main.css';
 
-  export let data: { gameResults?: any[] };
+	export let data: { gameResults?: any[] };
 
-  let teamNames: string;
-  $: teamNames = $selectedTeams.join(', ');
+	let teamNames: string;
+	$: teamNames = $selectedTeams.join(', ');
 
-  const { gameResults } = data;
+	const { gameResults } = data;
 
-  console.log('gameResults', gameResults);
+	console.log('gameResults', gameResults);
 </script>
 
 <section class="wrapper">
-  <section class="results-section">
-    <div class="results-container">
-      <h1>This Week's Results for 
-        <span class="team-names" class:light={!$theme} class:dark={$theme}>
-          {teamNames}
-        </span>
-      </h1>
+	<section class="results-section" class:light={!$theme} class:dark={$theme}>
+		<div class="results-container">
+			<h1>
+				This Week's Results for
+				<span class="team-names" class:light={!$theme} class:dark={$theme}>
+					{teamNames}
+				</span>
+			</h1>
 
-      {#if gameResults}
-        <div class="game-results-container">
-          {#each gameResults as { team, data } (team)}
-            {#each data as gameResult (gameResult.id)}
-            <div class="game-results" class:light={!$theme} class:dark={$theme}>
-              <h2>Week {gameResult.week} - {gameResult.season_type}</h2>
-              <p class="game-info">Start Date: {gameResult.start_date}</p>
-              <p class="game-info">Venue: {gameResult.venue}</p>
-              <p class="game-info">Home Team: {gameResult.home_team}</p>
-              <p class="game-info">Away Team: {gameResult.away_team}</p>
-              <p class="game-info">Conference Game: {gameResult.conference_game ? 'Yes' : 'No'}</p>
-              <p class="game-info">Completed: {gameResult.completed ? 'Yes' : 'No'}</p>
-              <p class="game-info">Home Points: {gameResult.home_points}</p>
-              <p class="game-info">Away Points: {gameResult.away_points}</p>
-              <!-- Add more fields as needed -->
-            </div>
-            {/each}
-          {/each}
-        </div>
-      {/if}
-    </div>
-  </section>
+			{#if gameResults}
+				<div class="game-results-container">
+					{#each gameResults as { team, data } (team)}
+						{#each data as gameResult (gameResult.id)}
+							<div class="game-results" class:light={!$theme} class:dark={$theme}>
+								<h2>Week {gameResult.week} - {gameResult.season_type}</h2>
+								<p class="game-info">Start Date: {gameResult.start_date}</p>
+								<p class="game-info">Venue: {gameResult.venue}</p>
+								<p class="game-info">Home Team: {gameResult.home_team}</p>
+								<p class="game-info">Away Team: {gameResult.away_team}</p>
+								<p class="game-info">
+									Conference Game: {gameResult.conference_game ? 'Yes' : 'No'}
+								</p>
+								<p class="game-info">Completed: {gameResult.completed ? 'Yes' : 'No'}</p>
+								<p class="game-info">Home Points: {gameResult.home_points}</p>
+								<p class="game-info">Away Points: {gameResult.away_points}</p>
+								<!-- Add more fields as needed -->
+							</div>
+						{/each}
+					{/each}
+				</div>
+			{/if}
+		</div>
+	</section>
 
-  <section class="search-section">
-    <ResubmitSearch />
-  </section>
+	<section class="search-section">
+		<ResubmitSearch />
+	</section>
 </section>
 
 <style>
-  :root {
+	:root {
 		--teams-color: #bb0000;
-    --teams-color-dark: #ff9195;
-  }
+		--teams-color-dark: #ff9195;
+	}
 
-  h1 {
-    font-size: 2.25rem;
-    line-height: 2.5rem;
-    margin-bottom: 2rem;
-    text-align: center;
-  }
+	h1 {
+		font-size: 2.25rem;
+		line-height: 2.5rem;
+		margin-bottom: 2rem;
+		text-align: center;
+	}
 
-  h2 {
-    font-size: 1.5rem;
-    line-height: 2rem;
-    text-decoration: underline;
-    text-underline-offset: 2px;
-    text-decoration-thickness: 1px;
-  }
+	h2 {
+		font-size: 1.5rem;
+		line-height: 2rem;
+		text-decoration: underline;
+		text-underline-offset: 2px;
+		text-decoration-thickness: 1px;
+	}
 
-  .wrapper {
-    width: 100vw;
-    height: 100vh;
-  }
+	.wrapper {
+		width: 100vw;
+		height: 100vh;
+	}
 
-  .results-section {
-    display: flex;
-    justify-content: center;
-    width: 100vw;
-    margin: 0;
-    padding: 0;
-    background-color: var(--background-color);
-    color: var(--text-color);
-  }
+	.results-section {
+		display: flex;
+		justify-content: center;
+		width: 100vw;
+		margin: 0;
+		padding: 0;
+		background-color: var(--background-color);
+		color: var(--text-color);
+	}
 
-  .results-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100vw;
-    height: 100%;
-  }
+	.results-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100vw;
+		height: 100%;
+	}
 
-  .game-results-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
+	.game-results-container {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
 
-  .game-results {
-    width: 42%;
-    text-align: center;
-		max-width: 20rem;
-    padding: 1rem;
-    margin: 2rem 3rem;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    box-sizing: border-box;
+	.game-results {
+		width: min-content;
+		text-align: center;
+		padding: 0.5rem 2rem 1.5rem 2rem;
+		margin: 0.5rem 3rem;
+		margin-bottom: 1.25rem;
+		border: 1px solid #ccc;
+		border-radius: 5px;
+		box-sizing: border-box;
 		box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-		padding: 1.5rem;
-    margin-bottom: 1.25rem;
-    background-color: var(--background-color);
-    color: var(--text-color);
-  }
+		background-color: var(--background-color);
+		color: var(--text-color);
+	}
 
-  .team-names {
-    color: var(--teams-color);
-  }
+	.team-names {
+		color: var(--teams-color);
+	}
 
-  .team-names.dark {
-    color: var(--teams-color-dark);
-  }
+	.team-names.dark {
+		color: var(--teams-color-dark);
+	}
 
-  .game-info {
-    margin-bottom: 5px;
-  }
+	.game-info {
+		margin-bottom: 5px;
+	}
 
-  .light {
-    background-color: #f9f9f9;
-    color: #1a202c;
-  }
+	.light {
+		background-color: #f9f9f9;
+		color: #1a202c;
+	}
 
-  .dark {
-    background-color: #1a202c;
-    color: #f9f9f9;
-  }
-  
+	.dark {
+		background-color: #1a202c;
+		color: #f9f9f9;
+	}
+
 	/* Media query for mobile devices */
 	@media (max-width: 768px) {
-    .wrapper {
-      width: 100%;
-      height: 100%;
-    }
+		.wrapper {
+			width: 100%;
+			height: 100%;
+		}
 
-    .results-section {
+		.results-section {
 			width: 100%;
 			margin: 0 auto;
-    }
+		}
 
 		.results-container {
 			width: 100%;
 		}
 
-    .game-results {
-      width: 100%;
-      margin: 1.5rem;
-    }
+		.game-results {
+			width: 100%;
+			margin: 1.5rem;
+		}
 
-    h1 {
-      font-size: 1.25rem;
-      line-height: 1.75rem;
-    }
+		h1 {
+			font-size: 1.25rem;
+			line-height: 1.75rem;
+		}
 
-    h2 {
-      font-size: 1.125rem;
-      line-height: 1.5rem;
-    }
-    .game-info {
-      font-size: 0.875rem;
-      line-height: 1.25rem;
-    }
-  }
+		h2 {
+			font-size: 1.125rem;
+			line-height: 1.5rem;
+		}
+		.game-info {
+			font-size: 0.875rem;
+			line-height: 1.25rem;
+		}
+	}
 </style>

f954da944286c8a118f2c38e3b93768ee8c11bbd
soundwanders <soundwanders@gmail.com>
2024-01-06 17:59:55 -0500

    fix: resubmit search functions properly

--- src/components/ResubmitSearch.svelte
+++ src/components/ResubmitSearch.svelte
@@ -1,127 +1,147 @@
-<!-- ResubmitSearch.svelte -->
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import fbsData from '../data/fbs.json';
-	import fcsData from '../data/fcs.json';
-	import { selectedTeams } from '$lib/stores/store';
-	import { theme } from '$lib/stores/theme';
-	import { getCurrentWeek } from '$lib/utils/getCurrentWeek';
-	import { goto } from '$app/navigation';
+  import { onMount } from 'svelte';
+  import fbsData from '../data/fbs.json';
+  import fcsData from '../data/fcs.json';
+  import { selectedTeams } from '$lib/stores/store';
+  import { theme } from '$lib/stores/theme';
+  import { getCurrentWeek } from '$lib/utils/getCurrentWeek';
+  import { getCurrentYear } from '$lib/utils/getCurrentYear';
+  import { goto, invalidateAll } from '$app/navigation';
 
-	let searchQuery: string = '';
-	let searchResults: string[] = [];
+  let searchQuery: string = '';
+  let searchResults: string[] = [];
 
-	let selectedWeek: number = 1;
+  let selectedWeek: number = 1;
+  let selectedYear: number = new Date().getFullYear();
 
-	let selectedTeamsArray = $selectedTeams;
+  let selectedTeamsArray = $selectedTeams;
 
-	const minQueryLength: number = 2;
+  const minQueryLength: number = 2;
 
-	function searchTeams() {
-		const query: string = searchQuery.toLowerCase();
+  function searchTeams() {
+    const query: string = searchQuery.toLowerCase();
 
-		if (query.length >= minQueryLength) {
-			searchResults = [...fbsData, ...fcsData].filter((team: string) => {
-				return team.toLowerCase().includes(query);
-			});
-		} else {
-			searchResults = [];
-		}
-	}
+    if (query.length >= minQueryLength) {
+      searchResults = [...fbsData, ...fcsData].filter((team: string) => {
+        return team.toLowerCase().includes(query);
+      });
+    } else {
+      searchResults = [];
+    }
+  }
 
-	function selectTeam(event: Event, team: string) {
-		event.preventDefault();
+  function selectTeam(event: Event, team: string) {
+    event.preventDefault();
 
-		// Toggle selected team in the selectedTeams store
-		selectedTeams.update((selectedTeams: string[]) => {
-			if (selectedTeams.includes(team)) {
-				return selectedTeams.filter((selectedTeam) => selectedTeam !== team);
-			} else {
-				return [...selectedTeams, team];
-			}
-		});
+    // Toggle selected team in the selectedTeams store
+    selectedTeams.update((selectedTeams: string[]) => {
+      if (selectedTeams.includes(team)) {
+        return selectedTeams.filter((selectedTeam) => selectedTeam !== team);
+      } else {
+        return [...selectedTeams, team];
+      }
+    });
 
-		// Trigger a re-render to update searchResults and highlight selected team
-		searchTeams();
-	}
+    // Trigger a re-render to update searchResults and highlight selected team
+    searchTeams();
+  }
 
-	// Update searchResults when the component mounts to handle pre-selected teams
-	onMount(() => {
-		searchTeams();
+  // Update searchResults when the component mounts to handle pre-selected teams
+  onMount(() => {
+    searchTeams();
 
-		// Check if there's a week parameter in the URL
-		const urlParams = new URLSearchParams(window.location.search);
-		const weekParam = urlParams.get('week');
+    // Check if there's a week parameter in the URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const weekParam = urlParams.get('week');
+    const yearParam = urlParams.get('year');
 
-		// Set selectedWeek based on the URL parameter or the current week
-		selectedWeek = weekParam ? parseInt(weekParam, 10) : getCurrentWeek();
+    // Set selectedWeek based on the URL parameter or the current week
+    selectedWeek = weekParam ? parseInt(weekParam, 10) : getCurrentWeek();
 
-		selectedTeamsArray = $selectedTeams;
-	});
+    // Set selectedYear based on the URL parameter or the current year
+    selectedYear = yearParam ? parseInt(yearParam, 10) : getCurrentYear();
 
-	$: {
-		selectedTeamsArray = $selectedTeams;
-	}
+    selectedTeamsArray = $selectedTeams;
+  });
 
-	function handleResubmit() {
-		// Use goto to navigate to the results page with the selected teams as a query parameter in the URL
-		goto(`/games?teams=${selectedTeamsArray.join(',')}&week=${selectedWeek}`);
-	}
+  $: {
+    selectedTeamsArray = $selectedTeams;
+  }
 </script>
 
-<div class="resubmit-container" class:light={!$theme} class:dark={$theme}>
-	<div class="container">
-		<div>
-			<label for="teamSearch">Search for a Different Team:</label>
-			<input
-				type="text"
-				id="teamSearch"
-				bind:value={searchQuery}
-				placeholder="Enter team name"
-				on:input={searchTeams}
-			/>
-		</div>
+<div class="resubmit-wrapper" class:light={!$theme} class:dark={$theme}>
+  <div class="container">
+    <div class="resubmit-container">
+      <label for="teamSearch">Search for a Different Team:</label>
+      <input
+        type="text"
+        id="teamSearch"
+        bind:value={searchQuery}
+        placeholder="Enter team name"
+        on:input={searchTeams}
+      />
+    </div>
 
-		<div>
-			<!-- Dropdown container for choosing which week to fetch -->
-			<label for="resubmit-week">Select Week:</label>
-			<select id="resubmit-week" bind:value={selectedWeek}>
-				{#each [...Array(17).keys()] as week}
-					<option value={week + 1}>{week + 1}</option>
-				{/each}
-			</select>
-		</div>
+    <div class="resubmit-container">
+      <!-- Dropdown container for choosing which week to fetch -->
+      <label for="resubmit-week">Select Week:</label>
+      <select id="resubmit-week" bind:value={selectedWeek}>
+        {#each [...Array(12).keys()] as week}
+          <option value={week + 1}>{week + 1}</option>
+        {/each}
+      </select>
+    </div>
 
-		<p id="search-query">
-			You searched for:
-			<span class="query-result">{searchQuery}</span>
-		</p>
+    <div class="resubmit-container">
+      <!-- Input for selecting the year -->
+      <label for="resubmit-year">Select Year:</label>
+      <input
+        type="number"
+        id="resubmit-year"
+        bind:value={selectedYear}
+        min={1900}
+        max={new Date().getFullYear()}
+      />
+    </div>
 
-		<div class="search-results">
-			{#if searchResults.length > 0}
-				<ul class="team-list">
-					{#each searchResults as team (team)}
-						<li class="team-list-items">
-							<button
-								class="teams-button"
-								on:mousedown={(event) => selectTeam(event, team)}
-								class:selected={$selectedTeams.includes(team)}
-								class:light={!$theme}
-								class:dark={$theme}
-								tabindex="0"
-							>
-								{team}
-							</button>
-						</li>
-					{/each}
-				</ul>
-			{:else if searchQuery.length >= minQueryLength}
-				<p>No teams found!</p>
-			{/if}
-		</div>
+    <p id="search-query">
+      You searched for:
+      <span class="query-result">{searchQuery}</span>
+    </p>
 
-		<button on:click={handleResubmit} class="resubmit-button"> Submit </button>
-	</div>
+    <div class="search-results">
+      {#if searchResults.length > 0}
+        <ul class="team-list">
+          {#each searchResults as team (team)}
+            <li class="team-list-items">
+              <button
+                class="teams-button"
+                on:mousedown={(event) => selectTeam(event, team)}
+                class:selected={$selectedTeams.includes(team)}
+                class:light={!$theme}
+                class:dark={$theme}
+                tabindex="0"
+              >
+                {team}
+              </button>
+            </li>
+          {/each}
+        </ul>
+      {:else if searchQuery.length >= minQueryLength}
+        <p>No teams found!</p>
+      {/if}
+    </div>
+		<a 
+			href={selectedTeamsArray.length > 0
+				? `/games?teams=${selectedTeamsArray.join(',')}&year=${selectedYear}&week=${selectedWeek}`
+				: '#'}
+			data-sveltekit-reload
+		>
+			<button type="button" class="resubmit-button" disabled={selectedTeamsArray.length === 0}>
+				Submit
+			</button>
+		</a>
+  </div>
 </div>
 
 <style module>
@@ -139,15 +159,23 @@
 		--highlight-color: #336699;
 	}
 
-	.resubmit-container {
+	.resubmit-wrapper {
 		width: 100%;
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		flex-direction: column;
-		padding: 1.25rem;
+		padding: 0 1rem;
 		background-color: var(--background-color);
 		color: var(--text-color);
+	}
+
+	.container {
+		margin-top: 2.5rem;
+	}
+
+	.resubmit-container {
+		margin-bottom: 1.5rem;
 	}
 
 	label {
@@ -212,6 +240,7 @@
 		padding: 0.25rem 0.675rem;
 		background-color: var(--primary-color);
 		color: #f9f9f9;
+		margin-bottom: 3rem;
 	}
 
 	.resubmit-button:hover {

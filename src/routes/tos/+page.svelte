b8c80bd3e883c039db89da00b4a781d1ada5c238
soundwanders <soundwanders@gmail.com>
2024-08-06 22:28:17 -0400

    Update Terms of Service

--- src/routes/tos/+page.svelte
+++ src/routes/tos/+page.svelte
@@ -13,44 +13,32 @@
 					<strong>Acceptance of Terms</strong><br />
 					By using the Fieldwing website, you agree to comply with these Terms of Service. If you do
 					not agree, please refrain from using the app. If you would like to discuss the Terms of Service,
-					please feel free to contact us.
+					or anything else related to Fieldwing, please contact me.
 				</li>
 
 				<li>
 					<strong>App Usage</strong><br />
-					Our web app is designed for personal, non-commercial use. Any unauthorized use or reproduction
+					This web app is designed for personal, non-commercial use. Any unauthorized use or reproduction
 					of the app's content is strictly prohibited.
 				</li>
 
 				<li>
 					<strong>Data Accuracy</strong><br />
-					While we strive to provide accurate information, we are not responsible for inaccuracies or
-					errors in the app's content. Users are encouraged to verify information independently.
+					While I strive to provide accurate information, I am not responsible for inaccuracies or
+					errors in the app's content. Users are encouraged to verify information independently, as
+					our data comes from a third-party API.
 				</li>
 
 				<li>
 					<strong>User Data Privacy Policy</strong><br />
-					It's important to note that we do not collect or store any personal information from users
-					who visit our site. We do not request or retain any user data, both in the browser or on our
-					servers.
-				</li>
-
-				<li>
-					<strong>Code of Conduct</strong><br />
-					Users must adhere to a respectful and positive code of conduct within the app. Any form of
-					harassment, abuse, or inappropriate behavior will result in account suspension.
-				</li>
-
-				<li>
-					<strong>Open-Source Code of Ethics</strong><br />
-					While the code for our College Football App is open-source and freely accessible, users are
-					prohibited from redistributing or claiming it as their own. If you'd like to use the code in
-					any way, please reach out and I'd be happy to discuss it!
+					It's important to note that Fieldwing does not collect or store any personal information from users
+					who visit the site. I will never request your data, or retain any user data, both in the browser and 
+					on the server.
 				</li>
 
 				<li>
 					<strong>NCAA Rights Disclaimer</strong><br />
-					We do not own any rights to NCAA teams, schools, logos, or other NCAA proprietary content.
+					I do not own any rights to NCAA teams, schools, logos, or other NCAA proprietary content.
 					All rights are reserved by the NCAA.
 					<br />
 					All content, trademarks, and intellectual property in the app are owned by us. Users are prohibited
@@ -58,18 +46,12 @@
 				</li>
 
 				<li>
-					<strong>Changes to Terms</strong><br />
-					We may update these Terms of Service from time to time. Users will be notified of any changes,
-					and continued use constitutes acceptance of the revised terms.
-				</li>
-
-				<li>
 					<strong>Contact Information</strong><br />
-					For any questions or concerns regarding these terms, please contact us.
+					For any questions or concerns regarding these terms, please contact me.
 				</li>
 			</ol>
 
-			<p>Thank you for using <strong>Fieldwing</strong>, we appreciate you!</p>
+			<p>Thank you for using <strong>Fieldwing</strong>, I appreciate you!</p>
 		</div>
 	</div>
 </section>
